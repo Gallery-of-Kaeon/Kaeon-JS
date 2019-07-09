0fa7e5be9ece25ee78b1b409eb69d6bd76faada6
Jesse Dinkin <kaeon.ace@gmail.com>
2019-07-09 14:11:32 -0400

    Kaeon

--- Kaeon United/Source/KaeonUnited.js
+++ Kaeon United/Source/KaeonUnited.js
@@ -1,86 +1,85 @@
 var module = {
-    id: '.',
-    exports: { },
-    parent: null,
-    filename: "",
-    loaded: false,
-    children: [],
-    paths: []
+	id: '.',
+	exports: { },
+	parent: null,
+	filename: "",
+	loaded: false,
+	children: [],
+	paths: []
 };
 
 function require(path) {
-    
-    require.cache = require.cache ? require.cache : [[], []];
 
-    if(module.parent != null) {
+	require.cache = require.cache ? require.cache : [[], []];
 
-        if(path.startsWith(".")) {
-            
-            path =
-                module.filename.substring(0, module.filename.lastIndexOf('/') + 1) +
-                path;
-        }
-    }
+	if(module.parent != null) {
+
+		if(path.startsWith(".")) {
+
+			path =
+				module.filename.substring(0, module.filename.lastIndexOf('/') + 1) +
+				path;
+		}
+	}
 
 	let lowerPath = path.toLowerCase();
 
 	while(lowerPath.startsWith("././"))
 		lowerPath = lowerPath.substring(2);
 
-    let index = require.cache[0].indexOf(lowerPath);
+	let index = require.cache[0].indexOf(lowerPath);
 
-    if(index == -1) {
+	if(index == -1) {
 
-        let rawFile = new XMLHttpRequest();
-        rawFile.open("GET", path, false);
+		let rawFile = new XMLHttpRequest();
+		rawFile.open("GET", path, false);
 
-        let allText = "";
+		let allText = "";
 
-        rawFile.onreadystatechange = function() {
+		rawFile.onreadystatechange = function() {
 
-            if(rawFile.readyState === 4) {
+			if(rawFile.readyState === 4) {
 
-                if(rawFile.status === 200 || rawFile.status == 0) {
-                    allText = rawFile.responseText;
-                }
-            }
-        }
+				if(rawFile.status === 200 || rawFile.status == 0)
+					allText = rawFile.responseText;
+			}
+		}
 
-        rawFile.send(null);
+		rawFile.send(null);
 
-        let newModule = {
-            id: path,
-            exports: { },
-            parent: module,
-            filename: path,
-            loaded: false,
-            children: [],
-            paths: []
-        };
+		let newModule = {
+			id: path,
+			exports: { },
+			parent: module,
+			filename: path,
+			loaded: false,
+			children: [],
+			paths: []
+		};
 
-        require.cache[0].push(lowerPath);
-        require.cache[1].push(newModule);
+		require.cache[0].push(lowerPath);
+		require.cache[1].push(newModule);
 
-        let newModuleContents = (
-            new Function(
-                "var module = arguments[0];" +
-                require.toString() +
-                "require.cache = arguments[1];" +
-                allText +
-                ";return module;"
-            )
-        )(newModule, require.cache);
+		let newModuleContents = (
+			new Function(
+				"var module = arguments[0];" +
+				require.toString() +
+				"require.cache = arguments[1];" +
+				allText +
+				";return module;"
+			)
+		)(newModule, require.cache);
 
-        for(key in newModuleContents)
-            newModule.exports[key] = newModuleContents.exports[key];
+		for(key in newModuleContents)
+			newModule.exports[key] = newModuleContents.exports[key];
 
-        module.children.push(newModule);
+		module.children.push(newModule);
 
-        newModule.loaded = true;
+		newModule.loaded = true;
 
-        return newModule.exports;
-    }
+		return newModule.exports;
+	}
 
-    else
-        return require.cache[1][index].exports;
+	else
+		return require.cache[1][index].exports;
 }
